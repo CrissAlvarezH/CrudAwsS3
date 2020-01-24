2b1fa263095c34be504d08b7d79451eb29c61797
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2020-01-24 16:37:51 -0500

    Add boto3 to files app

--- files/views.py
+++ files/views.py
@@ -1,5 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from testbackend.aws_s3_credentials.py import s3_data
+import boto3
+
+def get_s3_client():
+    s3Client = sessionS3.client(
+            service_name='s3',
+            region_name= s3_data['region_name'],
+            aws_access_key_id= s3_data['aws_access_key_id'],
+            aws_secret_access_key= s3_data['aws_secret_access_key']
+        )
 
 @api_view(['GET'])
 def status(request):
---
+++ testbackend/aws_s3_credentials.py
@@ -0,0 +1,5 @@
+s3_data = {
+    'aws_access_key_id': 'aws_access_key_id',
+    'aws_secret_access_key': 'aws_secret_access_key',
+    'region_name': 'region_name',
+}
