35c3a21166654c32ed84941544f1d8ebfba53499
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2020-01-24 16:42:10 -0500

    Create session s3

--- files/views.py
+++ files/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from testbackend.aws_s3_credentials.py import s3_data
 import boto3
 
+sessionS3 = boto3.session.Session()
+
 def get_s3_client():
     s3Client = sessionS3.client(
             service_name='s3',
